e4c727f486e3062345aa6a0f705f9518635e32ae
Yantimir <mingazitdinov@mail.ru>
2022-11-26 11:10:21 +0400

    реализация алгоритма решения задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[6] { "486", "-55", "samara", "russia", "computer science", "2" };
+string[] result = new string[array.Length];
+
+void NewArray(string[] array, string[] result)
+{
+  int count = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i].Length <= 3)
+    {
+      result[count] = array[i];
+      count++;
+    }
+  }
+}
+
+void PrintArray(string[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    Console.Write($"{array[i]} ");
+  }
+  Console.WriteLine();
+}
+
+NewArray(array, result);
+PrintArray(result);
